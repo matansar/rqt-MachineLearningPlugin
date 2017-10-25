fbd2caa6ab330e12037f9708e753f501ab7676a2
Matan Sar-Israel <matansar@post.bgu.ac.il>
2017-10-25 13:41:10 +0300

    commit

--- rqt_mlp/install/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Add here execution flags for the scripts.
-chmod +x ../scripts/rqt_mlp
-chmod +x ../src/rqt_mlp/Scenarios/Extentions/scripts/restart.sh
-chmod +x ../src/rqt_mlp/Scenarios/Extentions/scripts/clean.sh
-
