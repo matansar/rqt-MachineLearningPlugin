c24045dad7f83844d1ae9ad9776ed587bbbdab3f
rafaelkas <rafaelkasif@gmail.com>
2017-10-25 13:05:27 +0300

    change project name

---
+++ rqt_mlp/install/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Add here execution flags for the scripts.
+chmod +x ../scripts/rqt_mlp
+chmod +x ../src/rqt_mlp/Scenarios/Extentions/scripts/restart.sh
+chmod +x ../src/rqt_mlp/Scenarios/Extentions/scripts/clean.sh
+
--- rqt_mybag/src/rqt_mybag/Scenarios/Extentions/scripts/restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kill -9 `ps aux | grep ros | grep -v grep | awk '{print $2}'`
-kill -9 `ps aux | grep rqt | grep -v restart | grep -v grep | awk '{print $2}'`
-roscore &
-sleep 1
-rosrun rqt_mybag rqt_mybag
