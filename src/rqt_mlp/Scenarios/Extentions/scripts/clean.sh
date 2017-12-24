e6760853b5a1457d156119126035cf1ce656d895
Matan Sar-Israel <matansar@post.bgu.ac.il>
2017-12-24 20:41:14 +0200

    restart and clean script fixes

--- src/rqt_mlp/Scenarios/Extentions/scripts/clean.sh
+++ src/rqt_mlp/Scenarios/Extentions/scripts/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kill -9 `ps aux | grep ros | grep -v grep | awk '{print $2}'`
 kill -9 `ps aux | grep rqt | grep -v restart | grep -v grep | awk '{print $2}'`
-kill -9 `ps aux | grep rviz | grep -v restart | awk '{print $2}'`
+kill -9 `ps aux | grep rviz | grep -v restart | grep -v grep | awk '{print $2}'`
